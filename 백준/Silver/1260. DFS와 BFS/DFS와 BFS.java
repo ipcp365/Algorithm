b59b6972c18d8d6c0117a5db0f564f235b2566e5
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-06-14 03:04:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 180 ms, Memory: 21060 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,71 +1,71 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
 public class Main {
 	
 	static StringBuffer sb = new StringBuffer();
 	
-	static int node, line, start;
+	static int NODE;
+	static int LINE;
+	static int START;
 	
-	static int[][] arr;
+	static int arr[][];
 	static boolean[] check;
 	
-	static Queue<Integer> q = new LinkedList<Integer>();
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		node = Integer.parseInt(st.nextToken());
-		line = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(st.nextToken());
-		
-		arr = new int[node+1][node+1];
-		check = new boolean[node+1];
-		
-		for(int i=0; i<line; i++) {
-			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			
-			int a = Integer.parseInt(st2.nextToken());
-			int b = Integer.parseInt(st2.nextToken());
-			
-			arr[a][b] = arr[b][a] = 1;
-		}
-		
-		// DFS
-		dfs(start);
-		sb.append("\n");
-		
-		// BFS
-		check = new boolean[node+1];
-		bfs(start);
-		
-		// Result
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception{
+     
+    	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	   StringTokenizer st = new StringTokenizer(br.readLine());
+    	   
+    	   // 초기화 
+    	   NODE = Integer.parseInt(st.nextToken());
+    	   LINE = Integer.parseInt(st.nextToken());
+    	   START = Integer.parseInt(st.nextToken());
+    	   arr = new int[NODE+1][NODE+1];
+    	   check = new boolean[NODE+1];
+    	   
+    	   // input
+    	   for(int i=0; i<LINE; i++) {
+    		   st = new StringTokenizer(br.readLine());
+    		   
+    		   int a = Integer.parseInt(st.nextToken());
+    		   int b = Integer.parseInt(st.nextToken());
+    		   arr[a][b] = arr[b][a] = 1;
+    	   }
+    	   
+    	   // DFS
+    	   dfs(START);
+    	   sb.append("\n");
+    	   
+    	   // BFS 
+    	   check = new boolean[NODE+1];
+    	   bfs(START);
 
+   			// Result
+   			System.out.println(sb);
+    	   
+    }
 
 	private static void dfs(int start) {
 		sb.append(start + " ");
 		check[start] = true;
 		
-		// 재귀 호출
-		for(int i=1; i<=node; i++) {
+		// 재귀
+		for(int i=1; i<=NODE; i++) {
 			if(arr[start][i] == 1 && !check[i]) {
 				check[i] = true;
 				dfs(i);
 			}
 		}
 	}
-
-
+	
 	private static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		
 		q.add(start);
 		check[start] = true;
 		
@@ -73,14 +73,14 @@ public class Main {
 			start = q.poll();
 			sb.append(start + " ");
 			
-			for(int i=1; i<=node; i++) {
+			for(int i=1; i<=NODE; i++) {
 				if(arr[start][i] == 1 && !check[i]) {
 					q.add(i);
 					check[i] = true;
 				}
+				
 			}
 		}
+		
 	}
-	
-	
 }
