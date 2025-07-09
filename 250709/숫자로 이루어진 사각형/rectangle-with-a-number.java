0942fc916400d87d72f75046243e31cae27633a0
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-07-09 16:22:11 +0900

    Title: 숫자로 이루어진 사각형, Time: 241ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 250709/숫자로 이루어진 사각형/rectangle-with-a-number.java
+++ 250709/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -3,12 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 1;
 
         for(int i=0; i<n*n; i++){
             System.out.print((i%9+1) + " ");
 
-            if(i%n == 3) System.out.println("");
+            if((i+1)%4 == 0) System.out.println("");
         }
     }
 }
