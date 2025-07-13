3baf7507862995e43f26222ed9d258310712fe66
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-07-13 22:10:09 +0900

    [Bronze II] Title: 진수 변환기, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/14915. 진수 변환기/진수 변환기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		String answer = Integer.toString(M, N);
+		System.out.println(answer.toUpperCase());
+	}
+	
+}
