895512ff7022ee6d4aca4bfe492bcb8d7e93bb70
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-10-04 11:58:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.95 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,38 +1,16 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        }
 
-    	String[] answer = new String[n];
-    	
-    	// 정수를 이진수로 변환
-    	String[] first = new String[n];
-    	String[] second = new String[n];
-    	for(int i=0; i<n; i++) {
-    		String format = "%0" + n + "d";
-    		
-    		String binary = Integer.toBinaryString(arr1[i]);
-    		first[i] = String.format("%" + n + "s", binary).replace(' ', '0');
-    		
-    		binary = Integer.toBinaryString(arr2[i]);
-    		second[i] = String.format("%" + n + "s", binary).replace(' ', '0');
-    	}
-    	
-    	// 지도 그리기
-    	for(int i=0; i<n; i++) {
-    		String cur = "";
-    		
-    		for(int j=0; j<n; j++) {
-    			if(first[i].charAt(j) == '1' || second[i].charAt(j) == '1') {
-    				cur += "#";
-    			}else {
-    				cur += " ";
-    			}
-    		}
-    	
-    		// 넣기
-    		answer[i] = cur;
-    	}
-        
-        // Result
-        return answer;
+        for (int i = 0; i < n; i++) {
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
+        }
+
+        return result;
     }
 }
