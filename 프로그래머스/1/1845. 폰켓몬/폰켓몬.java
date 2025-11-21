344e19ee32b40d1a4604077f3cc8274df1f8a5fe
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-11-21 23:12:31 +0900

    [level 1] Title: 폰켓몬, Time: 5.58 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+/**
+[문제읽기]
+- N마리중 N/2 마리를 가져갈 수 있다.
+- 같은 종류의 포켓몬은 같은 번호를 가지고 있다.
+- 최대한 많은 종류의 포켓몬을 포함하여 N/2마리를 선택했을 때, 최종적으로 몇가지 종류의 포켓몬을 가질 수 있는가?
+*/
 class Solution {
     public int solution(int[] nums) {
-        // 초기화
+        
+        // 1. 데이터 누적
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        // 2. map 의 사이즈가 nums/2 이상이면 바로 정답
         int answer = 0;
-    	int maxCnt = nums.length/2;
-
-    	// 중복 제거
-    	Set<Integer> set = new HashSet<Integer>();
-    	for(int i=0; i<nums.length; i++) {
-    		set.add(nums[i]);
-    	}
-    	
-    	// Result
-        return set.size() > maxCnt ? maxCnt : set.size();
+        if(map.size() >= nums.length/2){
+            answer = nums.length/2;
+            return answer;
+        }else{
+            return map.size();
+        }
     }
 }
