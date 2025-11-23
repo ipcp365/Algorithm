55473a1a9a6145ec3d1edb932f02d6444ad4cba0
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-11-23 21:23:02 +0900

    [level 2] Title: 올바른 괄호, Time: 21.92 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 53.2 MB, 시간: 19.35 ms
+메모리: 54.9 MB, 시간: 21.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 06일 20:38:14
+2025년 11월 23일 21:22:58
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,33 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character> stack = new Stack<>();
         
+        
+        Stack<Character> stack = new Stack<>();
         for(char ch: s.toCharArray()){
+            
+            // [조건1] ( 는 순서에 상관없이 무조건 삽입
             if(ch == '('){
-                stack.add(ch);
-            }else{
-                if(!stack.empty() && stack.peek() == '('){
-                    stack.pop();
-                }else{
-                    answer = false;
-                    return answer;
-                }
+                stack.push(ch);
+                continue;
             }
+            
+            // [조건2] 비어있는 상태에서 닫는괄호가 주어질 경우 무조건 false
+            if(stack.isEmpty() && ch == ')') return false;
+            
+            // [조건3] 직전괄호가 여는 괄호이며, 이번 괄호가 닫히는 경우, 직전 괄호를 제거한다.
+            if(stack.peek() == '(' && ch == ')') {
+                stack.pop();
+            }else{
+                // [조건4] 여는 괄호 없이 닫힘괄호가 또 오는 경우 올바른 괄호가 아님 
+                return false;
+            }
+
         }
         
-        if(!stack.empty()) return false;
-
-        return answer;
+        
+        return stack.size() == 0 ? true : false;
     }
 }
