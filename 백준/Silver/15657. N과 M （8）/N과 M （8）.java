fe3241a609f30e65746352139f32433896afe1ea
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 21:29:10 +0900

    [Silver III] Title: N과 M (8), Time: 100 ms, Memory: 18988 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+	static int N, M;
+	static int[] nums;
+	static int[] answer;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken()); // 문자 수
+    	M = Integer.parseInt(st.nextToken()); // 길이
+    	nums = new int[N];
+    	visited = new boolean[N];
+    	answer = new int[M];
+    
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(nums);
+    	
+    	// Simulation
+    	dfs(0, 0);
+
+    	// Result
+    	System.out.println(sb.toString());
+    }
+
+	private static void dfs(int start, int depth) {
+		
+		// 종료 조건
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(answer[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 점화식
+		for(int i=start; i<N; i++) {
+			answer[depth] = nums[i];
+			dfs(i, depth+1);
+		}
+
+		
+	}
+}
