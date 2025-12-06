5ec7843b42ebd56e1a21297eae899bb86514d54c
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 12:04:44 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,44 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	
+
 	static int N, M;
 	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] nm = br.readLine().split(" ");
-		N = Integer.parseInt(nm[0]); // 1~n까지의 수 조합 
-		M = Integer.parseInt(nm[1]); // 길이(조합의 수가) M 이어야 함
-		arr = new int[M];
-		
-		getValues(1, 0);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        
+        // Simulation
+        dfs(0, 0);
 
-	private static void getValues(int at, int cnt) {
-		
-		// 기저 부분: 조건을 만족할 경우 바로 출
-		if(cnt == M) {
-			for(int i=0; i<arr.length; i++)
-				System.out.print(arr[i] + " ");
-			
-			System.out.println();
+        // Result
+        System.out.println(sb.toString());
+    }
+
+	private static void dfs(int start, int depth) {
+		// 종료 조건
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
 			return;
 		}
 		
-		// 유도 부분: 재귀함수를 계속 호출하는 부분
-		for(int i=at; i<=N; i++) {
-			
-			// 방문하지 않은 경우 재귀함수 호출 진행
-			arr[cnt] = i;
-			getValues(i+1, cnt+1);
+		// 점화식
+		for(int i=start; i<N; i++) {
+			arr[depth] = i+1;
+			dfs(i+1, depth+1);
 		}
-		
-		
-	}
+	}// ... dfs
+
+    
+
 }
