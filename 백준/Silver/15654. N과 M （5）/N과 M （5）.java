dafd3c18589cd02092ace0ee7d0ae5745e706636
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 14:45:23 +0900

    [Silver III] Title: N과 M (5), Time: 184 ms, Memory: 30996 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -5,22 +5,21 @@ import java.util.*;
 public class Main {
 
 	static int N, M;
+	static int[] arr;
 	static int[] nums;
-	static int[] answer;
 	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken()); // 총 개수
-        M = Integer.parseInt(st.nextToken()); // 최대 문자열 길이 (== depth)
-        nums = new int[N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
         visited = new boolean[N];
-        answer = new int[M];
         
         st = new StringTokenizer(br.readLine());
+        nums = new int[N];
         for(int i=0; i<N; i++) {
         	nums[i] = Integer.parseInt(st.nextToken());
         }
@@ -34,24 +33,25 @@ public class Main {
     }
 
 	private static void dfs(int depth) {
-		// 종료조건
+		// 종료 조건
 		if(depth == M) {
 			for(int i=0; i<M; i++) {
-				sb.append(answer[i]).append(" ");
+				sb.append(arr[i]).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		
 		// 점화식
-		for(int i=0; i<nums.length; i++) {
+		for(int i=0; i<N; i++) {
+			
 			if(!visited[i]) {
 				visited[i] = true;
-				answer[depth] = nums[i];
+				arr[depth] = nums[i];
 				dfs(depth+1);
 				visited[i] = false;
-				
 			}
+
 		}
 	}// ... dfs
 
