d2481cf6763eb31bc534fc6985f0c4ead4fff736
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 11:47:28 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 20928 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 
-/**
- * [문제읽기]
- * - N, M. 길이가 M 인 수열 
- * - 길이가 1부터 N 까지 자연수 중에서 중복없이 M개를 고른 수열
- */
 public class Main {
-	
+
 	static int N, M;
 	static boolean[] visited;
-    static int[] arr;
+	static int[] arr;
 	static StringBuilder sb = new StringBuilder();
 	
     public static void main(String[] args) throws IOException {
-    	
-    	// init & input
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-    	visited = new boolean[N+1];
-    	arr = new int[M];
-    	
-    	// Simulation
-    	dfs(0);
-    	
-    	// Result
-    	System.out.println(sb.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+        
+        // Simulation
+        dfs(0);
+
+        // Result
+        System.out.println(sb.toString());
     }
 
 	private static void dfs(int depth) {
-		// 종료조건 : 주어진 현재 문자(cur) 이 최대길이(M) 과 동일할 때
+		// 종료 조건
 		if(depth == M) {
 			for(int i=0; i<M; i++) {
 				sb.append(arr[i]).append(" ");
@@ -41,14 +34,17 @@ public class Main {
 			return;
 		}
 		
-		// 점화!
-		for(int i=1; i<=N; i++) {
+		// 점화식
+		for(int i=0; i<N; i++) {
 			if(!visited[i]) {
 				visited[i] = true;
-				arr[depth] = i;
+				arr[depth] = i+1;
 				dfs(depth+1);
 				visited[i] = false;
 			}
 		}
-	}
+	}// ... dfs
+
+    
+
 }
