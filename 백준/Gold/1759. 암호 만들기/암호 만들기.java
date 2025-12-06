ac87efb3d65f388b795a55faeed75def74afd17b
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 20:23:20 +0900

    [Gold V] Title: 암호 만들기, Time: 60 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,84 +1,69 @@
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 
-/**
- * 문제 읽기
- * - 암호는 서로 다른 L 개의 알파벳 소문자로 구성된다.
- *   최소 한개의 모음(a, e, i, o, u) + 두 개의 자음 으로 구성
- *  
- * - 암호를 이루는 알파벳이 암호에서 증가하는 순서로 배열되어 있을 것	
- *   abc 는 가능성 있지만, bac 는 그렇지 않음
- *   
- *   
- * - 새 보안 시스템에서 암호로 사용했을 법한 문자의 종류는 C 가지
- * - C개의 문자들이 모두 주어졌을 때, 가능성 있는 암호를 모두 구하여라!
- */
+
 public class Main {
 	
 	static int L, C;
-    static char[] arr;
-    static StringBuilder out = new StringBuilder();
+	static char[] words;
+	static char[] answer;
+	static StringBuilder sb = new StringBuilder();
 
+	
     public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	// init & input
-    	L = Integer.parseInt(st.nextToken());
-    	C = Integer.parseInt(st.nextToken());
-        arr = new char[C];
-
-    	st = new StringTokenizer(br.readLine());
-    	for(int i=0; i<C; i++) {
-    		arr[i] = st.nextToken().charAt(0);
-    	}
-    	Arrays.sort(arr);
-
-    	
-    	// Simulation
-        dfs(0, 0, 0, 0, new StringBuilder());
-    	
-    	// Result
-    	System.out.println(out);
-    }
-    
-    static boolean isVowel(char c){
-        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
-    }
-
-    // 선택할 문자 idx, 현재 문자열의 길이 len, 모음 v, 자음 cons
-    static void dfs(int idx, int len, int v, int cons, StringBuilder cur){
-    	
-    	// 종료 조건 : 전체 글자수에 도달 했을 경우 : 모음 1개 이상 & 자음 2개 이상 구성일 때에만 추가
-        if(len == L){
-            if(v >= 1 && cons >= 2) out.append(cur).append('\n');
-            return;
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        // 중단조건1 : 현재위치가 동일하여 (idx == C) 더이상 후보군이 없는 경우
-        if(idx == C) return;
+        L = Integer.parseInt(st.nextToken()); // 길이
+        C = Integer.parseInt(st.nextToken()); //문자의 수
+        words = new char[C];
+        answer = new char[L];
         
-        // 중단조건2 : 남은 후보 개수(len + (C-idx)) 가 목표길이(L) 보다 작은 경우, 
-        if(len + (C - idx) < L) return; // 남은 문자 수 부족 가지치기
-
-        for(int i=idx; i<C; i++){
-            char ch = arr[i];
-            cur.append(ch);
-            
-            // 재귀 : 모음/자음 여부 따져서 값 다르게 넘기기
-            if(isVowel(ch)) 
-            	dfs(i+1, len+1, v+1, cons, cur);
-            else            
-            	dfs(i+1, len+1, v, cons+1, cur);
-            
-            // // 방금 추가한 마지막 문자 제거 = 백트래킹
-            cur.deleteCharAt(cur.length()-1);
+        String[] str = br.readLine().split(" ");
+        for(int i=0; i<C; i++) {
+        	words[i] = str[i].charAt(0);
         }
+        Arrays.sort(words);
+        
+        /**
+         * Simulation 조건
+         * - 최소 한 개의 모음(a, e, i, o, u) & 최소 두 개의 자음
+         * - 정렬되어 있어야 함
+         */
+        dfs(0, 0);
+
+        // Result
+        System.out.println(sb.toString());
     }
+
+	private static void dfs(int depth, int start) {
+		
+		// 종료조건
+		if(depth == L) {
+			int vowel = 0;
+			int consonant = 0;
+			
+			for(int i=0; i<L; i++) {
+				if (isVowel(answer[i])) vowel++;
+				else consonant++;
+			}
+			
+			if(vowel >= 1 && consonant >= 2) {
+				sb.append(new String(answer)).append("\n");
+			}
+			
+			return;
+		}
+		
+		// 탐색
+		for(int i=start; i<C; i++) {
+			answer[depth] = words[i];
+			dfs(depth+1, i+1);
+		}
+	}// ... dfs
+	
+	private static boolean isVowel(char c) {
+		return c=='a' || c=='e' || c == 'i' || c=='o' || c=='u';
+	}
 }
