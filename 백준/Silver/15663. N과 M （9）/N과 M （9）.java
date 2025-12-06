0505d39cd384e86d8168c5bf06f2d3d92563478e
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-06 21:49:57 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+	static int N, M;
+	static int[] nums;
+	static int[] answer;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken()); // 문자 수
+    	M = Integer.parseInt(st.nextToken()); // 길이
+    	nums = new int[N];
+    	visited = new boolean[N];
+    	answer = new int[M];
+    
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0; i<N; i++) {
+    		nums[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(nums);
+    	
+    	// Simulation
+    	dfs(0);
+
+    	// Result
+    	System.out.println(sb.toString());
+    }
+
+    
+	private static void dfs(int depth) {
+		
+		// 종료 조건
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(answer[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 점화식
+		int prev = -1;
+		for(int i=0; i<N; i++) {
+			if(!visited[i] && prev != nums[i]) {
+				prev = nums[i];
+				
+				visited[i] = true;
+				answer[depth] = nums[i];
+				dfs(depth+1);
+				visited[i] = false;
+			}
+		}
+
+		
+	}
+}
