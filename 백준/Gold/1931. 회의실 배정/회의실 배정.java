c30b9b9a54e45b35ed0cab00e62f14a5dbc47a0b
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-12 23:10:45 +0900

    [Gold V] Title: 회의실 배정, Time: 604 ms, Memory: 59720 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,47 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    public static void main(String[] args) throws IOException {
-    	
-    	int n = Integer.parseInt(br.readLine());
-    	int[][] grid = new int[n][2];
-    	
-    	for(int i=0; i<n; i++) {
-    		String[] value = br.readLine().split(" ");
-    		grid[i][0] = Integer.parseInt(value[0]);
-    		grid[i][1] = Integer.parseInt(value[1]);
-    	}
-    	
-    	// 종료시간 기준으로 정렬 진행: 동일한 값이 있을 경우 시작시간을 기준으로 추가 조건
-    	// 회의시간이 짧은 시간, 혹은 시작 시간이 작은 순서로 할 경우 최적의 답을 구할 수 없음
-    	Arrays.sort(grid, (a, b) -> {
-    		if(a[1] == b[1]) {
-    			return a[0] - b[0];
-    		}
-    		return a[1] - b[1];
-    	});
-    	
-    	
-    	int cnt = 0;
-    	int lastEndTime = 0;
-    	
-    	// 회의 선택
-    	for(int i=0; i<n; i++) {
-    		if(grid[i][0] >= lastEndTime) {
-    			// 현재 회의 종료시간으로 업데이트
-    			lastEndTime = grid[i][1];
-    			cnt++;
-    		}
-    	}
-    	
-    	System.out.println(cnt);
-    	
+	static class Node implements Comparable<Node>{
+		int start, end;
+		
+		Node(int start, int end){
+			this.start = start;
+			this.end = end;
+		}
 
-    }
+		@Override
+		public int compareTo(Node o) {
+			if(this.end != o.end) {
+				return Integer.compare(this.end, o.end);
+			}
+			
+			// end 가 같을 경우 start 시간으로
+			return Integer.compare(this.start, o.start);
+		}
+	}
+	
+	static int N;
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		// init & input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		List<Node> schedule = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			String[] lesson = br.readLine().split(" ");
+			
+			schedule.add(new Node(Integer.parseInt(lesson[0]), Integer.parseInt(lesson[1])));
+		}
+		
+		Collections.sort(schedule);
+		
+		// Simulation
+		int answer = 1;
+		int lastIndex = schedule.get(0).end;
+		
+		for(int i=1; i<N; i++) {
+			if(lastIndex <= schedule.get(i).start) {
+				// 가능한 경우 새 수업을 듣는다.
+				lastIndex =  schedule.get(i).end;
+				answer++;
+			}else {
+				// 불가능 한 경우 i 값이 증가하기때문에 다음 수업이 가능한지 확인가능
+			}
+		}
+
+		// Result
+		System.out.println(answer);
+	}
 }
