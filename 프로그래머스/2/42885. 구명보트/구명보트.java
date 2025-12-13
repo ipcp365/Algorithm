e2ec2e453da47fe25d019e335fdcb79abf41953b
SaRang <41463572+ipcp365@users.noreply.github.com>
2025-12-13 21:32:20 +0900

    [level 2] Title: 구명보트, Time: 11.02 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,34 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-/**
-[문제 읽기]
-- 사람들을 구출하기 위해 필요한 최소한의 구명보트 개수
-- 구명보트는 최대 2명 (limit KG 제한) 이 있음
+/*
+[문제읽기]
+- 무게제한 있는(limit) 구명보트 + 최대 2명 탑승 가능
+- 구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 한다.
 */
 class Solution {
     public int solution(int[] people, int limit) {
         
-        // 1. 최적의 해를 찾기 위한 오름차순 정렬
+        // 정렬
         Arrays.sort(people);
         
-        // 2. 포인터 방식으로 진행
+        // 포인터 사용하기
         int answer = 0;
         int left = 0;
         int right = people.length - 1;
         
         while(left <= right){
             
-            // 가장 무거운 사람을 기준으로 태우기
+            // 보트 하나에 두명을 태울 수 있는 경우
             if(people[left] + people[right] <= limit){
                 left++;
                 right--;
-            } else{
-                // 같이 못타는 경우 : right가 너무 무거운 경우 혼자 타야함
+            }else{
+                // 보트 하나에 다 태울 수 없는 경우
                 right--;
             }
             
-            // 무조건 둘이 같이 타거나, 한명만 타는 결과가 나옴
+            // 가장 무거운 사람과 가장 가벼운 사람을 비교 했기 때문에, 위에서 2명을 동시에 태우지 못했다면
+            // 그 사람은 혼자 타야하는 운명!
             answer++;
         }
         
