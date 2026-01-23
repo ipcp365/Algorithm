4c587c2d1dd54dcc15b93f78f71c2faaba5f27ba
SaRang <41463572+ipcp365@users.noreply.github.com>
2026-01-23 15:40:11 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,85 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
 
-class Point{
-	int x, y;
-	
-	Point(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
-}
-
+/**
+ * [문제 읽기]
+ * - N*M 크기 배열 미로
+ * - 이동가능(1), 이동 불가능(0)
+ * - (1, 1) 에서 출발해서 (N, M) 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하여라 (단, 서로 인접한 칸으로만 이동할 수 있음)
+ */
 public class Main {
 	
-	static int N, M;
-	static int[][] arr;
-	static boolean[][] visit;
+	static int N, M, answer;
+	static int[][] map;
+	static int[] dxs = {-1, 0, 1, 0};
+	static int[] dys = {0, 1, 0, -1};
 	
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
+		// init & input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 맵의 크기 N * M
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		visit = new boolean[N][M];
+		map = new int[N][M];
+		answer = Integer.MAX_VALUE;
 		
-		// 입력값 받기
 		for(int i=0; i<N; i++) {
-//			StringTokenizer st2 = new StringTokenizer(br.readLine());
-		    String line = br.readLine(); // 공백 없이 한 줄 입력
+			String line = br.readLine();
 			for(int j=0; j<M; j++) {
-		        arr[i][j] = line.charAt(j) - '0'; // 문자 → 숫자 변환
+				map[i][j] = line.charAt(j) - '0';
 			}
 		}
-		
-		
-		// bfs 연산 수행
-		bfs(new Point(0,0));
-		
-		
-		// 출력 : (N, M) 까지 도달하는데 걸린 거리
-		System.out.println(arr[N-1][M-1]);
-	}
 
+		
+		// Simulation
+		bfs(0, 0);
 
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	private static void bfs(Point point) {
-		Queue<Point> que = new LinkedList<Point>(); 
-		que.add(new Point(point.x, point.y));
-		visit[point.x][point.y] = true;
 		
-		while(!que.isEmpty()) {
-			Point current = que.poll();
-			
-			for(int i=0; i<4; i++) {
-				int nx = current.x + dx[i];
-				int ny = current.y + dy[i];
-				
-				// 맵을 벗어나는 경우, 이미 방문 한 경우, 벽에 가로막힌 경우 탐색 불가 
-				if(nx < 0 || ny < 0 || nx >= N || ny >= M || visit[nx][ny] || arr[nx][ny] == 0) continue;
-				
-				
-				// 탐색 가능 한 경우!
-				arr[nx][ny] = arr[current.x][current.y]+1;
-				visit[nx][ny] = true;
-				que.add(new Point(nx, ny));
-			}
-		}
-		
+		// Result
+		System.out.println(map[N-1][M-1]);
 	}
 	
+	
+	static class Node{
+		int x, y;
+		
+		Node(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	private static void bfs(int row, int col) {
+		
+		Queue<Node> q = new ArrayDeque<>();
+		q.add(new Node(row, col));
+		
+		
+		// 반복
+		while(!q.isEmpty()) {
+			Node cur = q.poll();
+			
+			// 방향 탐색
+			for(int dir=0; dir<4; dir++) {
+				int nx = cur.x + dxs[dir];
+				int ny = cur.y + dys[dir];
+				
+				// 탐색 불가 조건
+				if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] != 1) continue;
+				
+				map[nx][ny] = map[cur.x][cur.y] + 1;
+				q.add(new Node(nx, ny));
+
+			}
+		}
+	}
+
 
 }
