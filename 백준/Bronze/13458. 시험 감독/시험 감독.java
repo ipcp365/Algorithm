19c3b5352048c298e3962985b4ccfb2ac2c2fece
SaRang <41463572+ipcp365@users.noreply.github.com>
2026-01-25 12:42:34 +0900

    [Bronze II] Title: 시험 감독, Time: 380 ms, Memory: 121604 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,37 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-
 /**
- * 문제 읽기
+ * [문제 읽기]
+ * - N개의 시험장, 각각의 시험장마다 응시자들이 있음
+ * - i번 시험장에 있는 응시자수는 Ai 명
+ * - 총감독관 : 한시험장에서 B명 감시 / 부감독관 : 한 시험장에서 C명 감시
  * 
- * - N 개의 시험장, 각 시험장에는 응시자가 있다.
- *   i 번째 시험장 응시자의 수는 Ai 명
- * - 총감독, 부감독관 있음.
- *   총감독 : 시험장당 감시 가능 응시자수 B명
- *   부감독관 : 시험장당 감시 가능 응시자수 C명
- * - 각 시험장마다 총감독관 1명(최대), 부감독관(여러명)
- *   모든 응시자를 감시한다고 할대, 필요한 감독관 수의 최솟값
- * 
+ * - 각 시험장에 총감독관은 오직 1명, 부감독관은 여러명 가능
+ * - 각 시험장마다 응시생들을 모두 감시해야 할 때, 필요한 감독관 수의 최솟값을 구하여라
  */
-
 public class Main {
 	
+	static int N, B, C;
+	static int[] students;
+	static int answer;
 
-    public static void main(String[] args) throws IOException {
-    	// init & input
-    	Scanner sc = new Scanner(System.in);
-    	int N = sc.nextInt();
-    	int[] arr = new int[N];
-    	for(int i=0; i<N; i++) {
-    		arr[i] = sc.nextInt();
-    	}
-    	
-    	int B = sc.nextInt(); // 총감독관 감시
-    	int C = sc.nextInt(); // 부감독관 감시
-    	
-    	
-    	// Simulation : 시험장 arr[i] 번째에 필요한 감독관 수 구하기
+	public static void main(String[] args) throws Exception {
+		
+		// init & input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		students = new int[N];
+		for(int i=0; i<N; i++) {
+			students[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		B = Integer.parseInt(st.nextToken()); // 총감독관이 한번에 관리할 수 있는 학생 수
+		C = Integer.parseInt(st.nextToken()); // 부감독관이 한번에 관리할 수 있는 학생 수
+		
+
+		// Simulation
     	long answer = 0;
     	for(int i=0; i<N; i++) {
     		
@@ -39,16 +41,19 @@ public class Main {
     		answer++;
     		
     		// 총감독관 한명으로 모두 감시할 수 없는 경우, 부감독관 필요
-    		if(arr[i] - B > 0) {
+    		if(students[i] - B > 0) {
     			// 감독받지 못하는 사람이 생기지 않도록 + C
     			// 부감독관이 과하게 필요하지 않도록 - 1
-    			answer += ((arr[i] - B) + C - 1) / C;
+    			answer += ((students[i] - B) + C - 1) / C;
     		}
     		
     	}
+		
+		
+		// Result
+		System.out.println(answer);
+	}
 
-    	
-    	// Result
-    	System.out.println(answer);
-    }
+
+
 }
